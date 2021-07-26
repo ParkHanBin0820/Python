e89964aa666727368089fe696b3f44c79f8ea72c
ParkHanBin0820 <qkrgksqls123542@naver.com>
2021-07-26 20:06:52 +0900

    Python

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-from bs4 import BeatilfulSoup
+#프로그래머스 같은숫자는 싫어
+
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == 0: #처음은 answer에 저장
+            answer.append(arr[i]) #answer에 arr list 저장
+        elif arr[i] != arr[i-1]: #arr의 전 값이 arr와 같이 않을 경우 
+            answer.append(arr[i]) #저장
+    return answer 
